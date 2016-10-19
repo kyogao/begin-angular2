a1bdd1c20f4894a3a1c6d513a587a9f787bbd5c4
kyogao <kyogao1983@qq.com>
2016-10-19 21:52:27 +0800

    update banner src
    add a hero propety

--- app/banner.component.ts
+++ app/banner.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({ 
 	selector: 'app-banner', 
-	template: '<h1>{{title}}</h1>' 
+	template: `
+		<h1>{{title}}</h1>
+		<a>{{hero}}</a>
+	` 
 }) 
 
 export class BannerComponent { 
-	title = 'Test Tour of Heroes'; 
+	title = 'Test Tour of Heroes';
+	hero = 'Superman is a hero'; 
 } 
